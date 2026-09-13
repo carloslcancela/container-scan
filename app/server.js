898433a3eb0e0de79785c47778a61bde9f884384
Carlos Lucas <122367934+carloslcancela@users.noreply.github.com>
2026-09-12 22:45:54 -0300

    Initialize Express server for DevSecOps Container Lab

---
+++ app/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    application: "DevSecOps Container Lab",
+    status: "running"
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}`);
+});
